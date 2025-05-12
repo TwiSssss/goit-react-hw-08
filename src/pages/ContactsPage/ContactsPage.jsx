8d5ed3baa9d0605d8556bacc30f21b2b39397e76
Vlad Tsokota <v.tsokota.69@gmail.com>
2025-05-12 10:34:12 +0300

    push2

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -3,16 +3,13 @@ import ContactForm from "../../components/ContactForm/ContactForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import ContactList from "../../components/ContactList/ContactList";
 import { fetchContacts } from "../../redux/contacts/operations";
-import { selectContacts } from "../../redux/contacts/selectors";
 import React, { useEffect } from "react";
 
 const Contacts = () => {
     const dispatch = useDispatch();
-    const contacts = useSelector(selectContacts);
     useEffect(() => {
-        if (contacts.length === 0) dispatch(fetchContacts());
-    }, [dispatch, contacts]);
-
+        dispatch(fetchContacts());
+    }, [dispatch]);
     return (
         <div>
             <h1>Phonebook</h1>
